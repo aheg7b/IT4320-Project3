ae46dac53dae3cc15540739b93a7343434ee5b03
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-10-30 20:48:58 -0500

    Added functions into the main program

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-#First push!
+#IT 4320 Project 3
+#Aidan Engbert
+#NAME
+#NAME
+#NAME
+
+def Stock_Name_Check(name):
+    return 0
+    #return 1 for valid input
+    #return 0 for invalid input
+
+def Chart_Type(chart_type):
+    return 0
+    #takes input of a number
+    #checks number for valid input
+    #returns 1 for valid input
+    #returns 0 for invalid input
+
+def Time_Series(time_type):
+    return 0
+    #takes input of a number
+    #checks number for valid input
+    #returns 1 for valid input
+    #returns 0 for invalid input
+
+def Dates(START,END):
+    return 0
+    #checks for vallid input
+    #suggest date swap for invalid input
+    #returns 1 for valid input
+    #returns 0 for invalid input
+
+def Graph(input):
+    return 0
+    #takes all the data and makes a graph
+
+def Open_Browser(input):
+    return 0
+    #opens the browser to view the chart
