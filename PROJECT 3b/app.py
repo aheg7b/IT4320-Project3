b4bef9577c086f41e41da3c3b9c9fee114e5473f
aheg7b <aheg7b@umsystem.edu>
2025-11-16 14:42:42 -0600

    added in functions in the python file

--- PROJECT 3b/app.py
+++ PROJECT 3b/app.py
@@ -1,3 +1,9 @@
+## IT 4320 Project 3b
+#Aidan Engbert
+#NAME
+#NAME
+#NAME
+
 import os
 import io
 from flask import Flask, render_template, request, send_from_directory, jsonify, url_for
@@ -7,3 +13,24 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from datetime import datetime
+
+API_KEY = "Y1VTX9XT399MJE42"
+BASE_URL = "https://www.alphavantage.co/query"
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
+def alphavantage_symbol_search(keywords):
+    return
+
+def alphavantage_daily_series(symbol, outputsize="compact"):
+    return
+
+def index():
+    return
+
+def search_symbols():
+    return
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG", "0") == "1")
